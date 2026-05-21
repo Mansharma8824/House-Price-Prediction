13bfdaa32d43502a3b8b93f459655fc3de72997e
Manish Kumar <manishsharma9675800189@gmail.com>
2026-05-22 01:25:20 +0530

    created model and deploying using flask

---
+++ app/app.py
@@ -0,0 +1,63 @@
+from flask import Flask, render_template,request
+import pickle
+import json
+import numpy as np
+
+app = Flask(__name__)
+
+# load the ML model
+model = pickle.load(open("models/house_price_pred_model.pkl","rb"))
+
+# Load colums 
+with open("models/columns.json","r") as f:
+    data_columns = json.load(f)["data_columns"]
+
+# Get locations columns from data_columns    
+locations = data_columns[4:]
+locations = [loc.replace("location_", "") for loc in locations]
+
+@app.route("/")
+def home():
+    return render_template('index.html', locations = locations)
+
+@app.route("/predict", methods = ['POST'])
+def price_predict():
+    # geting the values from the form
+    sqft = float(request.form['sqft'])
+    bath = float(request.form['bath'])
+    balcony = float(request.form['balcony'])
+    bhk = int(request.form['bhk'])
+    location = request.form["location"]
+    
+    # created vectors of columns
+    column_vector = np.zeros(len(data_columns))
+    column_vector[0] = sqft
+    column_vector[1] = bath
+    column_vector[2] = balcony
+    column_vector[3] = bhk
+    
+    # Create location column name
+    location_column = "location_" + location
+    
+    # check if filled location if available in columns
+    if location_column in data_columns:
+        loc_index = data_columns.index(location_column)
+        column_vector[loc_index] = 1
+    else:
+        other_index = data_columns.index("location_other")
+        column_vector[other_index] = 1
+        
+    # make Prediction
+    prediction = model.predict([column_vector])[0]
+
+    # get the result
+    return render_template(
+        "index.html",
+        prediction_text=f"Estimated Price: {round(prediction, 2)} Lakhs",
+        locations=locations
+    )
+    
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
